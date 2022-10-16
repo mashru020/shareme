eaf73ed979600294034a1a0a16b2d3943c7be4b8
mashru020 <mashrurrahman16@gmail.com>
2022-10-16 23:38:02 +0600

    create and config react, tailwind and make home and login component

---
+++ shareme_frontend/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Route } from 'react-router-dom';
+
+
+import App from './App'
+import './index.css';
+
+ReactDOM.render(
+    <Route>
+        <App/>
+    </Route>,
+    document.getElementById('root')
+);
